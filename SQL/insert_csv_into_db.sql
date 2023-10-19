626c656e892736ca66e9ea4e283fb03f211641af
Pablo Fajardo <pfajardodiaz17@gmail.com>
2023-10-19 11:11:52 -0500

    Adding SQL queries to upload performance logs

---
+++ SQL/insert_csv_into_db.sql
@@ -0,0 +1,28 @@
+-- Table Structure
+CREATE TABLE PERFORMANCE_LOG(
+    idProyecto varchar(4),
+    idPrueba varchar(2),
+    idRegion varchar(3),
+    idServidor int(2),
+    timeStamp bigint,
+    fecha datetime,
+    elapsed varchar(10),
+    label varchar(100),
+    responseCode varchar(3),
+    responseMessage varchar(20),
+    threadName varchar(100),
+    dataType varchar(20),
+    success varchar(15),
+    failureMessage varchar(50),
+    bytes varchar(30),
+    sentBytes varchar(30),
+    grpThreads varchar(30),
+    allThreads varchar(30),
+    URL varchar(200),
+    Latency varchar(30),
+    IdleTime varchar(30),
+    Connect varchar(30)
+);
+
+-- Insert CSV file into table
+LOAD DATA INFILE '/path/to/file' INTO TABLE PERFORMANCE_LOG FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES;
